094b2a75a6dfa3a74c1b9d89e8e3b5b4ba57095c
nicolasGibaud7 <nicolas.gibaud7@gmail.com>
2022-11-18 11:37:56 +0100

    Make functionnal_tests an app, use LiveServerTestCase

--- functionnal_tests/tests.py
+++ functionnal_tests/tests.py
@@ -1,11 +1,12 @@
 import time
 import unittest
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -20,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
@@ -40,7 +41,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(1)
         self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item. She
@@ -49,7 +50,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(1)
 
         self.check_for_row_in_list_table("1: Buy peacock feathers")
         self.check_for_row_in_list_table(
@@ -69,7 +70,3 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-
-
-if __name__ == "__main__":
-    unittest.main()
