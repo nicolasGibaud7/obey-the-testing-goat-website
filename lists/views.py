535285f373b2e645c420c90bda46588ff60bc92a
nicolasGibaud7 <nicolas.gibaud7@gmail.com>
2022-11-21 19:32:56 +0100

    Associate item with lists in model

--- lists/views.py
+++ lists/views.py
@@ -1,19 +1,21 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 
 
 def home_page(request: HttpRequest):
-    if request.method == "POST":
-        Item.objects.create(text=request.POST["item_text"])
-        return redirect("/lists/the-only-list-in-the-world/")
-
     return render(request, "home.html")
 
 
 def view_list(request: HttpRequest):
     items = Item.objects.all()
     return render(request, "list.html", {"items": items})
+
+
+def new_list(request: HttpRequest):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST["item_text"], list=list_)
+    return redirect("/lists/the-only-list-in-the-world/")
