e6088ad281ea674b9fcc313e40982ff16ccbff2e
NicolasGibaud7 <nicolas.gibaud7@gmail.com>
2023-04-18 09:11:20 +0200

    Change mail handling login tests

--- functionnal_tests/test_login.py
+++ functionnal_tests/test_login.py
@@ -1,11 +1,13 @@
+import os
+import poplib
 import re
+import time
 
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
 
 from .base import FunctionalTest
 
-TEST_EMAIL = "edith@example.com"
 SUBJECT = "Your login link for Superlists"
 
 
@@ -14,8 +16,13 @@ class LoginTest(FunctionalTest):
         # Edith goes to the awesome superlists site
         # and notices a "Log in" section in the navbar for the first time
         # It's telling her to enter her email address, so she does
+        if self.staging_server:
+            test_email = "ezc.tests@yahoo.com"
+        else:
+            test_email = "edith@example.com"
+
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name("email").send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name("email").send_keys(test_email)
         self.browser.find_element_by_name("email").send_keys(Keys.ENTER)
 
         # A message appears telling her an email has been sent
@@ -28,7 +35,7 @@ class LoginTest(FunctionalTest):
 
         # She checks her email and finds a message
         email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
+        self.assertIn(test_email, email.to)
         self.assertEqual(email.subject, SUBJECT)
 
         # It has a url link in it
@@ -43,16 +50,40 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # she is logged in!
-        self.wait_for(
-            lambda: self.browser.find_element_by_link_text("Log out")
-        )
-        navbar = self.browser.find_element_by_css_selector(".navbar")
-        self.assertIn(TEST_EMAIL, navbar.text)
+        self.wait_to_be_logged_in(email=test_email)
 
         # Now she logs out
         self.browser.find_element_by_link_text("Log out").click()
 
         # She is logged out
-        self.wait_for(lambda: self.browser.find_element_by_name("email"))
-        navbar = self.browser.find_element_by_css_selector(".navbar")
-        self.assertNotIn(TEST_EMAIL, navbar.text)
+        self.wait_to_be_logged_out(email=test_email)
+
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL("pop.mail.yahoo.com")
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ["YAHOO_PASSWORD"])
+            while time.time() - start < 60:
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print("getting msg", i)
+                    _, lines, _ = inbox.retr(i)
+                    lines = [l.decode("utf8") for l in lines]
+                    print(lines)
+                    if f"Subject: {subject}" in lines:
+                        email_id = i
+                        body = "\n".join(lines)
+                        return body
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
